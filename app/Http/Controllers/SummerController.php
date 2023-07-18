7d30b0f49f8c067d6149247c3cefd178feaefb43
metus01 <carnelmetus@gmail.com>
2023-07-18 13:25:10 +0100

    Ajout du controller

---
+++ app/Http/Controllers/SummerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SummerController extends Controller
+{
+    public function create()
+     {
+
+     }
+     public function store()
+     {
+        
+     }
+}
